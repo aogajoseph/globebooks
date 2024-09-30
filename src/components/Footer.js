7e88d9103a023307129e50c8527fa1128c673752
aogajoseph <aogathesecond@gmail.com>
2024-09-30 11:16:36 +0300

    adding footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,62 @@
 import React from "react";
+import { FaYoutubeSquare } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import Logo from '../images/logo.png';
+import Terms from '../images/Terms.pdf';
+import Privacy from '../images/Privacy.pdf';
+import Security from '../images/Security.pdf';
+import Cookie from '../images/Cookie.pdf';
 import '../css/Footer.css';
 
 const Footer = () => {
     return (
-        <p className="footer">Footer content (footer logo, product description, important links, contact details and social media icons) will appear here.</p>
+        <div className="footer">
+            <div className="footer section-padding">
+                <div className="footer-links">
+                    <div className="footer-description">
+                        <img src={Logo} alt='Logo' className="footer-logo" />
+                        <p className="footer-description">Experience creativity with Globebooks, a library of eBooks, audiobooks and animated videos. Globebooks is operated by Globe Technologies.</p>
+                    </div>
+                    <div className="footer-links-div">
+                        <h4>Important Links</h4>
+                        <a href="register" className="important-links"><p>Create Account</p></a>
+                        <a href="signin" className="important-links"><p>Sign In</p></a>
+                    </div>
+                    <div className="footer-links-div">
+                        <h4>Get in Touch</h4>
+                        <p>Remote</p>
+                        <p>Nairobi, Kenya.</p>
+                        <a href="mailto:globeheadquarters@gmail.com" className="important-links"><p>globeheadquarters@gmail.com</p></a>
+                    </div>
+                    <div className="footer-links-div">
+                        <h4>Stay Connected</h4>
+                        <div className ="social-media">
+                            <FaYoutubeSquare className="youtube social-links"/>
+                            <FaFacebookSquare className="facebook social-links" />
+                            <FaSquareXTwitter className="x social-links" />
+                            <FaLinkedin className="linkedin social-links" />
+                        </div>
+                    </div>
+                </div>
+
+                <hr></hr>
+
+                <div className="footer-below">
+                    <div className="copyright">
+                        <p>@{new Date().getFullYear()} Globe Technologies. All rights reserved.</p>
+                    </div>
+                    <div className="legal">
+                        <a href={Terms} target="_blank" rel="noopener noreferrer"><div><p>Terms & Conditions</p></div></a>
+                        <a href={Cookie} target="_blank" rel="noopener noreferrer"><div><p>Cookie Declaration</p></div></a>
+                        <a href={Privacy} target="_blank" rel="noopener noreferrer"><div><p>Privacy</p></div></a>
+                        <a href={Security} target="_blank" rel="noopener noreferrer"><div><p>Security</p></div></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
-}
+};
 
 export default Footer;
